cac084ed071e2b823d4d9d80f2dc24d44727856f
cristineshipman <cristine.shipman@gmail.com>
2017-08-04 15:43:20 -0500

    pulling data from moive api

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -2,13 +2,18 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+// config app
+app.set("view engine", "ejs");
+
 app.get("/results", function(req, res){
-  res.send("Hello, it works!");
+  request("http://www.omdbapi.com/?s=h&apikey=thewdb", function(error, response, body){
+    if (!error && response.statusCode == 200){
+      var data = JSON.parse(body);
+      res.render("results", {data: data});
+    }
+  });
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("Movie App has started!!!");
+app.listen(3000, function(){
+  console.log("magic is happening")
 });
-
-
-// http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb
